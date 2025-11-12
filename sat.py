00f8e57bce24cf12850c814d4e05bb66078d0905
cody.howell <cody.howell@students.snow.edu>
2025-11-11 21:07:12 -0700

    Verification is done!

--- sat.py
+++ sat.py
@@ -3,7 +3,19 @@ def sat(func: list):
     pass
 
 def verify_sat(func: list, solution: dict):
-    pass
+    for or_gate in func:
+        inputs = or_gate.split(";")
+        works = False
+        for i in inputs:
+            if len(i) == 2:
+                if solution[i[1]] == False:
+                    works = True
+            else:
+                if solution[i] == True:
+                    works = True
+        if not works:
+            return False
+    return True
 
 
 
--- test_sat.py
+++ test_sat.py
@@ -1,4 +1,24 @@
+from sat import verify_sat, sat
 
+def test_two_gates_1():
+    f = ['a', 'b']
+    d = {'a': True, 'b': True}
+    assert verify_sat(f, d) == True
 
-def test_thing():
-    pass
+def test_two_gates_2():
+    f = ['a', '-b']
+    d = {'a': True, 'b': False}
+    assert verify_sat(f, d) == True
+    d = {'a': True, 'b': True}
+    assert verify_sat(f, d) == False
+
+def test_two_gates_3():
+    f = ['a;c', 'b;-d']
+    d = {'a': True, 'b': True, 'c': True, 'd': True}
+    assert verify_sat(f, d) == True
+    d = {'a': False, 'b': True, 'c': True, 'd': False}
+    assert verify_sat(f, d) == True
+    d = {'a': False, 'b': True, 'c': False, 'd': True}
+    assert verify_sat(f, d) == False
+    d = {'a': True, 'b': False, 'c': True, 'd': True}
+    assert verify_sat(f, d) == False
