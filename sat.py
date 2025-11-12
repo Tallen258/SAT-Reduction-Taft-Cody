ba2af2f9b74d9428d3cce6a37bdba45023255101
Cody <codyhowell44@gmail.com>
2025-11-12 12:10:27 -0700

    added notes

--- sat.py
+++ sat.py
@@ -1,4 +1,4 @@
-from pulp import LpVariable, LpProblem, LpMaximize, value
+from pulp import LpVariable, LpProblem, LpMaximize, value, LpBinary
 
 def verify_sat(func: list, solution: dict):
     for or_gate in func:
@@ -24,13 +24,14 @@ def sat(func: list):
             inputs.add(c[-1])
 
     nodeToVariable = {
-        node: LpVariable(node, 0, 1, cat='Binary')
+        node: LpVariable(node, 0, 1, cat=LpBinary)
         for node in inputs
     }
     notNodeToVariable = {
-        node: LpVariable("not" + node, 0, 1, cat='Binary')
+        node: LpVariable("not" + node, 0, 1, cat=LpBinary)
         for node in inputs
     }
+
     for i in inputs:
         problem += nodeToVariable[i] + notNodeToVariable[i] == 1
 
@@ -40,7 +41,7 @@ def sat(func: list):
     
     gates = []
     for gate in func:
-        or_gate = LpVariable(gate.replace(';', ',').replace('-', 'not'), 0, 1)
+        or_gate = LpVariable(gate.replace(';', ',').replace('-', 'not'), 0, 1, cat=LpBinary)
         gates.append(or_gate)
         current_inputs = gate.split(';')
         current_variables = []
@@ -54,7 +55,7 @@ def sat(func: list):
             problem += or_gate >= variable
         problem += or_gate <= sum(current_variables)
 
-    f_variable = LpVariable("func", 0, 1)
+    f_variable = LpVariable("func", 0, 1, cat=LpBinary)
 
     for gate in gates:
         problem += f_variable <= gate
@@ -69,8 +70,17 @@ def sat(func: list):
         for i in inputs
     }
 
+# current knowledge: 
+# We know the objective function is being set to maximize our output variable
+# EVERYTHING is being typed as 'Integer'
+# Bounds are set by default between 0 and 1
+# Intermediate steps are returning 0.5
 
+# Console log below: 
+# Result - Linear relaxation infeasible
 
+# No feasible solution found
+# (this is for the circuit ['a;b', '-a;b', 'a;-b'], which has an obvious solution)
 
 
 f = ['a;b;c', '-a;b;-c', '-d;-b', '-a;c', '-b;a']
